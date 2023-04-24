e4661918666653d03a28b2c39a63bdbb91b54233
zchhhhh <2069820192@qq.com>
2023-04-24 11:34:03 +0800

    feat: 新增聊天功能

---
+++ chat-test/src/main/java/cdu/zch/payload/BroadcastMessageRequest.java
@@ -0,0 +1,16 @@
+package cdu.zch.payload;
+
+import lombok.Data;
+
+/**
+ * @author Zch
+ **/
+@Data
+public class BroadcastMessageRequest {
+
+    /**
+     * 消息内容
+     */
+    private String message;
+
+}
